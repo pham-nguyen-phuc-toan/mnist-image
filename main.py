6fcb09862ed35ec741b6467b5c9ce2929eefa642
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-12 20:59:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
     if st.button('Predict'):
         image = image.resize((8*8, 1))
         feature_vector = np.array(image)
-        label = str((model.predict(feature_vector)))
+        label = str((model.predict(feature_vector))[0])
 
         st.header('Result')
         st.text(label)
